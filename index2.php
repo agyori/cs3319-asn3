14492523d0057e7609d22f5604fc23fd3570a598
KenBradfield <kbradfi2@uwo.ca>
2014-11-27 18:15:13 -0500

    index

---
+++ index2.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Western ta database</title>
+</head>
+<body>
+<?php
+include 'connectdb.php';
+?> 
+<h1>Welcome to the Western Database</h1>
+<h2>Teach Assitants are people too</h2>
+</body>
+</html>
